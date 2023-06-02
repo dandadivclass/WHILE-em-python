2ec1fa00ecacb0bae774ff2be4a4afe91ff95ad7
Dandara <122398596+dandadivclass@users.noreply.github.com>
2023-06-01 23:31:34 -0300

    Create sobre_while.py
    Código que explica o funcionamento do While

---
+++ sobre_while.py
@@ -0,0 +1,6 @@
+#o while funciona como um bloco de código que vai se repetir SE uma condição for verdadeira
+
+i = 1                   #variável contadora (o loop vai começar a partir desse número)
+while i < 6 or i == 6:      #condição ao lado do while sempre
+    print(i)
+    i += 1              #variável incrementadora (o loop vai rodar a partir da contadora + incremento)
